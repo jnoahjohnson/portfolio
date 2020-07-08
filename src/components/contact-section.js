fccc49b8cc92bef6cefb15b0256908a96161e40c
Noah Johnson <jnoahjohnson@gmail.com>
2020-07-07 18:07:46 -0600

    Add success page

--- src/components/contact-section.js
+++ src/components/contact-section.js
@@ -13,6 +13,7 @@ const ContactSection = () => {
         method="post"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
+        action="/success"
       >
         <input type="hidden" name="bot-field" />
         <input type="hidden" name="form-name" value="contact" />
